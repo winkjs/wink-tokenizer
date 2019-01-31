9d99f484b3d953df85228b14a7d495c1fb7f10bf
Sanjaya Kumar Saxena <sanjaya@graype.in>
2019-01-31 20:54:50 +0530

    chore: update copyright year

--- test/eng-contractions-specs.js
+++ test/eng-contractions-specs.js
@@ -1,7 +1,7 @@
 //     wink-tokenizer
 //     Multilingual tokenizer that automatically tags each token with its type.
 //
-//     Copyright (C) 2017-18  GRAYPE Systems Private Limited
+//     Copyright (C) 2017-19  GRAYPE Systems Private Limited
 //
 //     This file is part of “wink-tokenizer”.
 //
